8a2e8d1653a27ada00d265564511476e74e2817d
Zifan Si <112644628+ZifanSi@users.noreply.github.com>
2024-04-07 13:15:31 -0400

    Feature-01-turnOpposite Closes #1

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -51,4 +51,22 @@ public enum Direction {
         }
         throw new IllegalStateException("Unexpected value: " + this);
     }
+
+    public Direction turnOpposite() {
+        switch (this) {
+            case UP -> {
+                return DOWN;
+            }
+            case DOWN -> {
+                return UP;
+            }
+            case LEFT -> {
+                return RIGHT;
+            }
+            case RIGHT -> {
+                return LEFT;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + this);
+        }
+    }
 }
