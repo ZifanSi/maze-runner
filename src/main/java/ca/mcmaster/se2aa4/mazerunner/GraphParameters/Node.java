70e0d896356fadc3c1cfb252ba7f7468c8ca9646
Zifan Si <112644628+ZifanSi@users.noreply.github.com>
2024-04-08 04:46:26 -0400

    Feature-03-Node

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/GraphParameters/Node.java
@@ -0,0 +1,35 @@
+package ca.mcmaster.se2aa4.mazerunner.GraphParameters;
+
+import java.util.Objects;
+
+
+public class Node {
+    public int x, y;
+    public Node parent;
+
+    /**
+     * Constructs a new Node with specified coordinates and parent node.
+     *
+     * @param x      The x-coordinate of the node
+     * @param y      The y-coordinate of the node
+     * @param parent The parent node in the path
+     */
+    public Node(int x, int y, Node parent) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node node = (Node) o;
+        return x == node.x && y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
